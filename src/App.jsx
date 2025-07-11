7d0f2ae93a5e6866f74bedb646187eb835673252
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-11 15:09:33 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Home from './pages/home/Home';
 import { RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RootLayout';
 import NotFound from './pages/not-found/NotFound';
-import MealItems from './pages/meal_item/MealItems';
-import Meal from './pages/meal/Meal';
 
 export default function App() {
   const router = createBrowserRouter([
@@ -15,15 +13,6 @@ export default function App() {
         {
           index: true,
           element: <Home />,
-        },
-        {
-          path: 'meal-items/:category',
-          element: <MealItems />
-
-        },
-        {
-          path: 'meal/:id',
-          element: <Meal />
         },
 
         {
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,57 +1,8 @@
-import { Avatar, Button, Typography } from '@material-tailwind/react';
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'
-import { useNavigate } from 'react-router';
-import { useApi } from '../../hooks/useApi';
+import React from 'react'
+
 export default function Home() {
-  const nav = useNavigate();
-
-  const [data, load, err] = useApi('https://www.themealdb.com/api/json/v1/1/categories.php');
-
-
-
-
-  if (load) {
-    return <div>
-      <Skeleton count={3} />
-      <br />
-      <Skeleton count={3} />
-      <br />
-      <Skeleton count={3} />
-      <br />
-      <Skeleton count={3} />
-      <br />
-      <Skeleton count={3} />
-
-    </div>
-  }
-
-
   return (
     <div>
-
-
-      {data && data.categories.map((cata) => {
-        return <div
-          onClick={() => nav(`/meal-items/${cata.strCategory}`)}
-          key={cata.idCategory} className='mb-7 cursor-pointer'>
-          <div className='flex items-center gap-5'>
-            <Avatar src={cata.strCategoryThumb} />
-            <Typography >{cata.strCategory}</Typography>
-
-          </div>
-          <p className='text-gray-600 text-sm line-clamp-3'>{cata.strCategoryDescription}</p>
-
-
-        </div>
-      })}
-
-
-
-
-
 
     </div>
   )
