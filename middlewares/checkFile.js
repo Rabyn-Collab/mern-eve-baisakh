8fd1fb74098c9048336498251303b3e4aa4ecd31
Rabyn-Collab <babynshrestha64@gmail.com>
2025-07-30 15:55:31 +0545

    changes

--- middlewares/checkFile.js
+++ middlewares/checkFile.js
@@ -1,18 +1,18 @@
 import path from 'path';
-
+import { v4 as uuidv4 } from 'uuid';
 const supportedExtesionType = ['.webp', '.jpg', 'jpeg', '.png', '.gif']
 
 export const checkFile = (req, res, next) => {
   const image = req.files?.image;
-  console.log(image);
 
   if (image) {
     const extensionType = path.extname(image.name);
     if (supportedExtesionType.includes(extensionType)) {
+      const imagePath = `${uuidv4()}-${image.name}`;
 
-      image.mv(`./uploads/${image.name}`, (err) => {
+      image.mv(`./uploads/${imagePath}`, (err) => {
         if (err) res.status(400).json({ message: 'something went wrong' });
-        req.imagePath = image.name;
+        req.imagePath = imagePath;
         next();
       })
 
