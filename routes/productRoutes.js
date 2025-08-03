2eeb61e92304f4eaad272d5a2ec56917cf51f862
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-03 15:41:03 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { addProduct, getProduct, getProducts, removeProduct, updateProduct } from '../controllers/productController.js';
 import { checkFile, updateCheckFile } from '../middlewares/checkFile.js';
 import { adminCheck, userCheck } from '../middlewares/authCheck.js';
+import { productSchema, validate } from '../utils/validators.js';
 
 const router = express.Router();
 
 
-router.route('/').get(getProducts).post(userCheck, adminCheck, checkFile, addProduct);
+router.route('/').get(getProducts).post(userCheck, adminCheck,
+  validate.body(productSchema), checkFile, addProduct);
 router.route('/:id').get(getProduct).patch(userCheck, adminCheck, updateCheckFile, updateProduct).delete(userCheck, adminCheck, removeProduct);
 
 
--- utils/validators.js
+++ utils/validators.js
@@ -5,6 +5,19 @@ import joivalidate from 'express-joi-validation';
 export const validate = joivalidate.createValidator({});
 
 
+export const productSchema = Joi.object({
+  title: Joi.string().min(10).max(100).required(),
+  description: Joi.string().min(10).required(),
+  stock: Joi.number().required(),
+
+  category: Joi.string().valid('men\'s clothing', 'women\'s clothing', 'electronics', 'beauty').required(),
+
+  brand: Joi.string().valid('nike', 'addidas', 'samsung', 'iphone', 'gucci', 'sunsilk', 'himalayan').required(),
+  price: Joi.number().required(),
+
+
+});
+
 export const registerSchema = Joi.object({
   username: Joi.string().min(5).max(40).required(),
   email: Joi.string().email().required(),
