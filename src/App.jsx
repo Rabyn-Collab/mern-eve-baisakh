c8d10b5487f1b78c987e29e58bbb118cafe84d77
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-03 15:45:00 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RootLayout';
 import NotFound from './features/not-found/NotFound';
-import ArticleList from './features/articles/ArticleList';
-import ArticleForm from './features/articles/ArticleForm';
-import UpdateArticleForm from './features/articles/UpdateArticleForm';
-
 
 export default function App() {
   const router = createBrowserRouter([
@@ -15,16 +11,9 @@ export default function App() {
       children: [
         {
           index: true,
-          element: <ArticleList />
+
         },
-        {
-          path: 'article-form',
-          element: <ArticleForm />
-        },
-        {
-          path: 'update-article-form/:id',
-          element: <UpdateArticleForm />
-        },
+
 
         {
           path: '*',
