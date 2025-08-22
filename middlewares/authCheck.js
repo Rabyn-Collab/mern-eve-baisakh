3ec3debacf9b41e0967ca0bb1565793da6bfb4be
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-22 15:36:57 +0545

    changes

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -3,9 +3,11 @@ import jwt from 'jsonwebtoken';
 
 
 export const userCheck = (req, res, next) => {
-  const token = req.headers.authorization;
-  console.log(req.cookies);
-  console.log(req.headers);
+  //const token = req.headers.authorization;
+  const token = req.cookies.jwt;
+  console.log(token);
+  // console.log(req.cookies);
+  // console.log(req.headers);
   const decode = jwt.decode(token, 'secret');
   if (!decode) return res.status(401).json({ message: 'unauthorized' });
   req.userId = decode.id;
