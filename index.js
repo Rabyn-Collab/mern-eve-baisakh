d459c8014011b189e65a1d39192f24108eba9cad
Rabyn-Collab <babynshrestha64@gmail.com>
2025-08-22 15:30:25 +0545

    changes

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ mongoose.connect('mongodb+srv://dbUser:dbuser@cluster0.nxeztd6.mongodb.net/Shop'
   console.log(err);
 });
 
-app.use(cookieParser());
+
 app.use(cors({
   origin: ['http://localhost:5173', 'https://mern-eve-baisakh.vercel.app'],
   credentials: true
 }));
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.static('uploads'))
 app.use(fileUpload({
--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 export const userCheck = (req, res, next) => {
   const token = req.headers.authorization;
   console.log(req.cookies);
+  console.log(req.headers);
   const decode = jwt.decode(token, 'secret');
   if (!decode) return res.status(401).json({ message: 'unauthorized' });
   req.userId = decode.id;
