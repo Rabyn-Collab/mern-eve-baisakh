460ac3e060e035c5551487db36f20d4d99fb1599
Rabyn-Collab <babynshrestha64@gmail.com>
2025-09-15 15:19:17 +0545

    changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,14 @@
-import Header from "../components/Header.jsx";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import AuthProvider from "./providers/authProviders.js";
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div>
-          <AuthProvider>
-            <Header />
-            <Toaster />
-            {children}
-          </AuthProvider>
-        </div>
+
+        {children}
+
 
       </body>
-    </html>
+    </html >
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,51 +1,13 @@
-import axios from 'axios'
 import React from 'react'
-import {
-  Card,
-  CardAction,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import Link from 'next/link.js';
-import RemoveEmployee from '../components/RemoveEmployee.jsx';
-import Image from 'next/image.js';
-import { getServerSession } from 'next-auth';
-import { options } from './api/auth/[...nextauth]/options.js';
 
-//export const dynamic = "force-dynamic";
-//export const revalidate = 5;
-
-export default async function Page() {
-
-
-
+export default function Page() {
   return (
-    <div className='p-5'>
-      <h1>This is Next Js</h1>
-      {/* 
-      <Image
-        loading='lazy'
-        src={'https://plus.unsplash.com/premium_photo-1703389454028-f53d91316e58?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8'}
-        height={200}
-        width={300}
-      /> */}
-      {/* {data.map((employee) => {
-        return <Card key={employee.id} className={'mb-5'}>
-          <CardHeader>
-            <CardTitle>{employee.fullname}</CardTitle>
-            <CardDescription>{employee.position}</CardDescription>
-            <CardAction>{employee.age}</CardAction>
-          </CardHeader>
+    <div>
 
-          <CardFooter className={'flex gap-5'}>
-            <Link href={`/form/edit/${employee.id}`}>Edit</Link>
-            <Link href={`/employees/${employee.id}`}>Go to profile</Link>
-            <RemoveEmployee id={employee.id} />
-          </CardFooter>
-        </Card>
-      })} */}
+      <h1>This is hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut assumenda magnam debitis labore totam repellat officiis dolor, modi nisi dolorem. Asperiores deserunt eum perferendis, sed dolor culpa repudiandae iste molestiae?</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo blanditiis voluptatibus optio dolorum libero vel atque voluptates rerum molestias reprehenderit, consequuntur totam similique impedit. Quidem velit quas incidunt magnam? Expedita.</p>
 
     </div>
   )
